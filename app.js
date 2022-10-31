b9185a27a79870fe438dc3c3f55e441ba4b005b2
BenBonno <benoit.bonnoron@gmail.com>
2022-10-31 18:54:47 +0100

    Exo Env ok

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ const express = require('express')
 const app = express()
 const port = process.env.SERVER_PORT
 
+const name = process.env.MY_NAME
+const city = process.env.MY_CITY
+const langage = process.env.MY_LANGUAGE
+
+
 app.listen(port, () => console.log(`Notre appli est accessible à l'URL :  http://localhost:${port}`))
 
 app.get("/", (req, res) => {
-  res.send(`I am ${process.env.MY_NAME}, wilder in ${process.env.CITY}, and I love ${process.env.MY_LANGAGE}`)
-  console.log(req.params);
+  res.send(`I am ${name}, wilder in ${city}, and I love ${langage}`)
 })
